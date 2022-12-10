7d9942ddc526200cdb8a3cba41c67b678e7bb417
VipersEye <nyk190@mail.ru>
2022-12-11 02:07:28 +0300

    Add styles for hidden nav
    main.css
    Add styles for hidden navigation.
    
    home.js
    Add new App class and event listener for navigation toggler button.

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,2 +1,18 @@
 import '@styles/main.css';
 import Database from '@modules/Database';
+
+class App {
+    constructor(Database) {
+        this.database = new Database();
+
+        let navTogglerBtn = document.querySelector('.nav__toggler-btn');
+        navTogglerBtn.addEventListener('click', (e) => {
+            let navContainer = document.querySelector('.nav-container');
+            navContainer.classList.toggle('nav-container_closed');
+
+            e.currentTarget.classList.toggle('nav__toggler-btn_open');
+        });
+    }
+}
+
+const app = new App(Database);
