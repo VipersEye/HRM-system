4628bb43a315c0c94e7a2ea9e8818ed638adf4b2
VipersEye <nyk190@mail.ru>
2022-12-11 18:32:30 +0300

    Add new tasks section and styles for it
    home.html
    Add structure of tasks section.
    
    main.css
    Add styles for tasks section
    
    circleProgress.js
    Add new file for circle diagrams.
    
    home.js
    Add code for creating circle diagrams with fake data.
    
    icons.svg
    Add new arrow icon.
    
    .eslintignore
    Update eslint ignore file, add circleProgress.js

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,7 +1,8 @@
 import '@styles/main.css';
+import CircleProgress from '@modules/CircleProgress';
 import Database from '@modules/Database';
 
-class App {
+class Home {
     constructor(Database) {
         this.database = new Database();
 
@@ -12,7 +13,16 @@ class App {
 
             e.currentTarget.classList.toggle('nav__toggler-btn_open');
         });
+
+        const diagrams = document.querySelectorAll('.main__task-diagram');
+        diagrams.forEach((diagram) => {
+            const circleDiagram = new CircleProgress(diagram, {
+                value: Math.floor(Math.random() * 100),
+                max: 100,
+                textFormat: 'percent'
+            });
+        });
     }
 }
 
-const app = new App(Database);
+const home = new Home(Database);
