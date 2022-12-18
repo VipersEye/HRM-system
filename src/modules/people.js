cf282daf46d02cb0aacbb18a9c248a0171bcbcc9
VipersEye <nyk190@mail.ru>
2022-12-18 13:57:00 +0300

    Add creating persona section for workers.
    people.html
    Move persona block to template element, delete fake content.
    
    main.css
    Rename css variables with colors, add new color.
    Add styles for hiding part of persona in case, when worker didn't
    complete FIRO-B test.
    Add new style for alert compatibility of workers.
    
    icons.svg
    Rename icons.
    
    package.json
    Install new package for drawing charts.
    
    FIRO-B.json
    Create new json file with data about FIRO-B test.
    
    people.js
    Move logic responsible for fetching workers data to new getWorkersData
    class method.
    Add event listener in createCards method for creating persona.
    Add new createPersona method for creating persona.
    Add new createChart method for creating radar chart.
    
    login.test.js
    Delete file.

--- src/modules/people.js
+++ src/modules/people.js
@@ -1,6 +1,8 @@
 import '@styles/main.css';
 import Database from '@modules/Database';
 import cytoscape from 'cytoscape';
+import Chart from 'chart.js/auto';
+import FIRO from '@modules/FIRO-B';
 
 class People {
     constructor(database) {
@@ -129,18 +131,27 @@ class People {
         });
     }
 
-    async createCards() {
+    async getWorkersData() {
         let workers = await this.database.select('worker');
         let divisions = await this.database.select('division');
 
-        let divisionsNameMap = new Map([...divisions].map((division) => [division.division_id, division.name]));
+        let divisionsNameMap = new Map([...divisions].map((division) => [+division.division_id, division.name]));
         workers.sort((workerFirst, workerSecond) => workerFirst.worker_id - workerSecond.worker_id);
         workers.forEach((worker) => {
+            worker.worker_id = +worker.worker_id;
             worker.fullname = `${worker.surname} ${worker.name}`;
+            worker.age = `${Math.floor((new Date() - new Date(Date.parse(worker.birthdate))) / 3.154e10)} лет`;
+            worker.division_id = +worker.division_id;
             worker.division = divisionsNameMap.get(worker.division_id);
             worker.salary = `${worker.salary}₽`;
+            worker.firo = JSON.parse(worker.firo);
         });
 
+        return workers;
+    }
+
+    async createCards() {
+        let workers = await this.getWorkersData();
         let template = document.querySelector('#worker-template');
         workers.forEach((worker) => {
             let workersContainer = document.querySelector('.content');
@@ -155,8 +166,159 @@ class People {
                 if (rowData) rowData.textContent = worker[prop];
             }
 
+            let moreBtn = workerCard.querySelector('.card__btn_more');
+            moreBtn.addEventListener('click', () => {
+                this.createPersona(worker.worker_id);
+            });
+
             workersContainer.append(workerCard);
         });
+    }
+
+    async createPersona(id) {
+        let workers = await this.getWorkersData();
+        let worker = workers.find((worker) => worker.worker_id === id);
+        let personaTemplate = document.querySelector('#persona-template');
+        let persona = personaTemplate.content.cloneNode(true).querySelector('.persona');
+        let container = document.querySelector('.container');
+
+        console.log(worker);
+
+        const setPersonaInfo = () => {
+            let avatar = persona.querySelector('.persona__avatar');
+            avatar.src = worker.avatar || './images/avatars/default-avatar.png';
+            for (let prop in worker) {
+                let elem = persona.querySelector(`[persona-${prop}]`);
+                if (elem) elem.textContent = worker[prop];
+            }
+        };
+
+        setPersonaInfo();
+
+        let closeBtn = persona.querySelector('.persona__btn');
+        closeBtn.addEventListener('click', () => {
+            persona.remove();
+        });
+
+        if (!worker.firo) {
+            persona.classList.add('persona_no-charts');
+            container.append(persona);
+            return;
+        }
+
+        const setPersonaCompatibility = () => {
+
+            const coworkers = workers.filter((coworker) => coworker.division_id === worker.division_id && coworker.worker_id !== worker.worker_id && coworker.firo !== null);
+            const compatibilities = coworkers.map((coworker) => {
+                let compatibility = {
+                    worker,
+                    coworker,
+                    firo: {
+                        I: 0,
+                        C: 0,
+                        A: 0
+                    }
+                };
+
+                let incompatibility = 0;
+                for (let key in compatibility.firo) {
+                    compatibility.firo[key] = Math.abs(worker.firo[key].e - coworker.firo[key].w) + Math.abs(coworker.firo[key].e - worker.firo[key].w);
+                    if (compatibility.firo[key] < 6) incompatibility++;
+                }
+
+                compatibility.status = incompatibility === 0 ? 'success' : incompatibility === 1 ? 'alert' : 'error';
+
+                return compatibility;
+            });
+
+            let compatibilityElem = persona.querySelector('.persona__compatibility');
+            let compatibilityIcon = persona.querySelector('.persona__compatibility__icon use');
+            let compatibilityText = persona.querySelector('.persona__status');
+
+            if (compatibilities.find((comp) => comp.status === 'error')) {
+                compatibilityElem.classList.add('persona__compatibility_error');
+                compatibilityIcon.setAttribute('xlink:href', './images/icons/svg/icons.svg#error-icon');
+                compatibilityText.textContent = 'Высокий риск несовместимости сотрудников';
+            } else if (compatibilities.find((comp) => comp.status === 'alert')) {
+                compatibilityElem.classList.add('persona__compatibility_alert');
+                compatibilityIcon.setAttribute('xlink:href', './images/icons/svg/icons.svg#question-icon');
+                compatibilityText.textContent = 'Риск несовместимости сотрудников';
+            } else {
+                compatibilityElem.classList.add('persona__compatibility_success');
+                compatibilityIcon.setAttribute('xlink:href', './images/icons/svg/icons.svg#success-icon');
+                compatibilityText.textContent = 'Полная совместимость сотрудников';
+            }
+        };
+
+        const setPersonaStats = () => {
+            let score = [];
+            for (let prop in worker.firo) {
+                score.push(...Object.values(worker.firo[prop]));
+                document.documentElement.style.setProperty(`--${prop}`, worker.firo[prop].e);
+
+                for (let key in worker.firo[prop]) {
+                    let description = persona.querySelector(`[scale="${prop + key}"]`);
+                    description.textContent = `${prop}${key}: ${FIRO.meaning[`${prop}${key}`][worker.firo[prop][key] < 5 ? 'low' : 'high']}`;
+                }
+            }
+
+            container.append(persona);
+            const radarContainer = persona.querySelector('#radar');
+            this.createChart(radarContainer, score);
+        };
+
+        setPersonaCompatibility();
+
+        setPersonaStats();
+    }
+
+    createChart(ctx, score) {
+        const data = {
+            labels: [
+                'Ie',
+                'Iw',
+                'Ce',
+                'Cw',
+                'Ae',
+                'Aw',
+            ],
+            datasets: [{
+                label: 'Баллы по шкалам',
+                data: score,
+                fill: true,
+                backgroundColor: 'rgba(100, 39, 241, 0.4)',
+                borderColor: 'rgb(100, 39, 241)',
+                pointBackgroundColor: 'rgb(100, 39, 241)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgb(100, 39, 241)'
+            }, {
+                label: '',
+                data: [0, 0, 0, 9, 9, 9],
+                fill: true,
+                backgroundColor: 'transparent',
+                borderColor: 'transparent',
+                pointBackgroundColor: 'transparent',
+                pointBorderColor: 'transparent',
+                pointHoverBackgroundColor: 'transparent',
+                pointHoverBorderColor: 'transparent'
+            }]
+        };
+
+        const config = {
+            type: 'radar',
+            data: data,
+            options: {
+                elements: {
+                    line: {
+                        borderWidth: 4
+                    }
+                }
+            },
+        };
+
+        Chart.defaults.font.size = 16;
+        new Chart(ctx, config);
     }
 }
 
