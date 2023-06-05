54192db11e2c18bd892e7fc28004eaa4cc19a50e
VipersEye <nyk190@mail.ru>
2023-06-05 15:15:07 +0300

    Add the functionality of adding a test to the DB
    testing.html
    Add input for adding new test files.
    
    testing.css
    Add new css file for testing page.
    
    testing.js
    Add addTest method that read content of file and add it to DB.

--- src/modules/testing.js
+++ src/modules/testing.js
@@ -2,8 +2,7 @@ import '@styles/default.css';
 import '@styles/main.css';
 import '@styles/section.css';
 import '@styles/sorting-fields.css';
-import '@styles/data.css';
-import '@styles/management.css';
+import '@styles/testing.css';
 
 import Database from '@modules/Database';
 import Events from '@modules/events';
@@ -13,13 +12,35 @@ class Testing {
 		this.database = new Database();
 		this.events = new Events(this.database);
 
-        const navTogglerBtn = document.querySelector('.nav__toggler-btn');
+		const navTogglerBtn = document.querySelector('.nav__toggler-btn');
 		const toggleNav = (e) => {
 			const navContainer = document.querySelector('.nav-container');
 			navContainer.classList.toggle('nav-container_closed');
 			e.currentTarget.classList.toggle('nav__toggler-btn_open');
 		};
 		navTogglerBtn.addEventListener('click', toggleNav);
+
+		const inputFile = document.querySelector('input[type="file"]');
+		inputFile.addEventListener('change', this.addTest.bind(this));
+	}
+
+	async addTest(e) {
+		const [file] = e.target.files;
+
+		const reader = new FileReader();
+		reader.onload = async () => {
+			try {
+				const fileString = reader.result;
+                const values = {
+                    data: fileString
+                };
+                await this.database.insert('test', values);
+				alert('Тест успешно добавлен');
+			} catch (error) {
+				alert('Произошла ошибка при чтении файла');
+			}
+		};
+		reader.readAsText(file);
 	}
 }
 
